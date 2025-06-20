a45cbfa6925355ab3c07136534b633f9a835e0b5
marianrl <marianoh.rodriguez@davinci.edu.ar>
2025-06-20 18:31:11 -0300

    Eliminamos mas librerias en conflicto con la build en eas

--- components/HapticTab.tsx
+++ components/HapticTab.tsx
@@ -1,15 +1,12 @@
 import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
 import { PlatformPressable } from '@react-navigation/elements';
-import * as Haptics from 'expo-haptics';
 
 export function HapticTab(props: BottomTabBarButtonProps) {
   return (
     <PlatformPressable
       {...props}
       onPressIn={(ev) => {
-        if (process.env.EXPO_OS === 'ios') {
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-        }
+        // Haptic feedback removed to fix production build issues
         props.onPressIn?.(ev);
       }}
     />
